407227c003bd99300b36100672ffe15ee6f88eb6
DSARRAZY <d.sarrazy@gmail.com>
2020-08-08 14:44:27 +0200

    install first admin

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Form\RegistrationFormType;
+use App\Form\UserType;
+use App\Security\LoginFormAuthenticator;
+use App\Security\UserAuthenticationAuthenticator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class RegistrationController extends AbstractController
+{
+
+    /**
+     * @Route("/install", name="app_admin")
+     */
+    public function registerAction(
+        Request $request,
+        UserPasswordEncoderInterface $passwordEncoder,
+        UserRepository $userRepository
+    ) :Response {
+
+        $users = $userRepository->findAll();
+        if (!empty($users)) {
+            return $this->redirectToRoute("home_index");
+        }
+
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $user->setRoles(["ROLE_ADMIN"]);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Votre compte à bien été enregistré.');
+
+            return $this->redirectToRoute('home_index');
+        }
+        return $this->render('registration/register_admin.html.twig', ['form' => $form->createView()]);
+    }
+}
